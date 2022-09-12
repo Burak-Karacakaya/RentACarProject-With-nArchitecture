28ac8dfd74941df0494f9c8b0f86d0ddd7f5436b
burak karacakaya <burakaracakaya@gmail.com>
2022-09-12 19:09:21 +0300

    Lesson 3 Ok();

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -13,7 +13,8 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
-       
+        public DbSet<Model> Models { get; set; }
+
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -24,16 +25,34 @@ namespace Persistence.Contexts
         {
             //if (!optionsBuilder.IsConfigured)
             //    base.OnConfiguring(
-            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("RentACarConnectionString")));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+
             modelBuilder.Entity<Brand>(a =>
             {
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasMany(p => p.Models); // 1 markanın 1'den fazla model'i var.
+            });
+
+            modelBuilder.Entity<Model>(a =>
+            {
+                a.ToTable("Models").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.BrandId).HasColumnName("BrandId");
+                a.Property(p => p.Name).HasColumnName("Name");
+                a.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+
+                a.HasOne(p => p.Brand); // model'in 1 tane markası var !!!
+
+
             });
 
 
@@ -41,6 +60,8 @@ namespace Persistence.Contexts
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes"), new(3, "Seat"), new(4, "Peugeot") }; //test datası oluşturması için
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
 
+            Model[] modelEntitySeeds = { new(1, 1, "Series 4", 990, ""), new(2, 1, "Series 3", 650, ""), new(3, 3, "Series A180", 1200, "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
            
         }
     }
